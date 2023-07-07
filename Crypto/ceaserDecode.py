7a07a61401d441e697184a34fbc921336121cb3f
Ионидис Екатерина <arseniy_sitnikov@mail.ru>
2023-07-07 19:32:06 +0300

    Minor chaneg

--- Crypto/ceaserDecode.py
+++ Crypto/ceaserDecode.py
@@ -1,4 +1,4 @@
-message = input("Enter encoded message:")
+message = input("Enter encoded message please:")
 SYMBOLS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890 !?.'
 for key in range(len(SYMBOLS)):
     translated = ''
